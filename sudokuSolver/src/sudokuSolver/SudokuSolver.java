c065460f379f39d39877b02064795e759920d3ef
Tim Norman <tim@timnorman.ca>
2023-03-17 11:33:15 -0400

    Added JavaDocs comments, including explanation of  solveBoard method.
    Signed-off-by: Tim Norman <tim@timnorman.ca>

--- sudokuSolver/src/sudokuSolver/SudokuSolver.java
+++ sudokuSolver/src/sudokuSolver/SudokuSolver.java
@@ -31,7 +31,13 @@ public class SudokuSolver {
 		board.printBoard(puzzle);
 	}
 	
-	
+	/**
+	 * Checks if a number is already present in the current row. Returns true if so, false otherwise.
+	 * @param board The 2D array representing the Sudoku board. 
+	 * @param number The number being tested. 
+	 * @param row The row being searched. 
+	 * @return
+	 */
 	private static boolean isNumberInRow(int[][] board, int number, int row) {
 		for (int i = 0; i < GRID_SIZE; i++) {
 			if (board[row][i] == number) {
@@ -41,6 +47,13 @@ public class SudokuSolver {
 		return false;
 	}
 
+	/**
+	 * Checks if a number is already present in the current column. Returns true if so, false otherwise. 
+	 * @param board The 2D array representing the Sudoku board. 
+	 * @param number The number being tested. 
+	 * @param column The column being searched. 
+	 * @return
+	 */
 	private static boolean isNumberInColumn(int[][] board, int number, int column) {
 		for (int i = 0; i < GRID_SIZE; i++) {
 			if (board[i][column] == number) {
@@ -50,6 +63,14 @@ public class SudokuSolver {
 		return false;
 	}
 
+	/**
+	 * Checks if a number is already present in the 3x3 sub-square of the board. Returns true if so, false otherwise.
+	 * @param board The 2D array representing the Sudoku board. 
+	 * @param number The number being tested. 
+	 * @param row The row being searched. Limited to the 3x3 sub-square of the board. 
+	 * @param column The column being searched. Limited to the 3x3 sub-square of the board. 
+	 * @return
+	 */
 	private static boolean isNumberInBox(int[][] board, int number, int row, int column) {
 		int localBoxRow = row - row % 3;
 		int localBoxColumn = column - column % 3;
@@ -64,12 +85,29 @@ public class SudokuSolver {
 		return false;
 	}
 	
+	/**
+	 * Tests that the number being tested is valid in the current position. It must pass the tests checking the row, column, and sub-square. 
+	 * @param board The 2D array representing the Sudoku board. 
+	 * @param number The number being testetd. 
+	 * @param row The row being searched. 
+	 * @param column The column being searched. 
+	 * @return
+	 */
 	private static boolean isValidPlacement(int[][] board, int number, int row, int column) {
 		return !isNumberInRow(board, number, row) &&
 				!isNumberInColumn(board, number, column) &&
 				!isNumberInBox(board,number, row, column);
 	}
 	
+	/**
+	 * A method to solve the Sudoku board using brute force. It iteratively tries each number (1-9) to determine 
+	 * if it is valid, and the first valid candidate is used. This method is then recursively called again to fill 
+	 * in the next blank, and so on. If a valid candidate cannot be found the last recursive loop is exited and the 
+	 * previous candidate is discarded and the next tried. This recursive process continues until all empty spaces 
+	 * have been filled. 
+	 * @param board The 2D array representing the Sudoku board. 
+	 * @return
+	 */
 	private static boolean solveBoard(int[][] board) {
 		for (int row = 0; row < GRID_SIZE; row++) {
 			for (int column = 0; column < GRID_SIZE; column++) {
